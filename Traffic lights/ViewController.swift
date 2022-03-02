914dd3321517029d8c0e9f18472bb4bba6a1b717
Konstantin Fomenkov <kolij2005@gmail.com>
2022-03-02 21:02:32 +0300

    release

--- Traffic lights/ViewController.swift
+++ Traffic lights/ViewController.swift
@@ -1,47 +1,77 @@
-//
-//  ViewController.swift
-//  Traffic lights
-//
-//  Created by Konstantin Fomenkov on 01.03.2022.
-//
+    //
+    //  ViewController.swift
+    //  Traffic lights
+    //
+    //  Created by Konstantin Fomenkov on 01.03.2022.
+    //
 
 import UIKit
 
 class ViewController: UIViewController {
-
-    enum ActiveView {
+    enum ActiveLight {
         case red, yellow, green
     }
 
-    @IBOutlet var lightsLabels: [UIView]!
+    @IBOutlet weak var redLightLabel: UIView!
+    @IBOutlet weak var yellowLightLabel: UIView!
+    @IBOutlet weak var greenLightLabel: UIView!
+
     @IBOutlet weak var nextButton: UIButton!
 
-    private var activeView = ActiveView.red
+    private var activeLight = ActiveLight.red
+    private var lightOn: CGFloat = 1
+    private var lightOff: CGFloat = 0.3
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        ligthsConfiguration()
         nextButton.configuration = nextButtonConfiguration()
+        initialViewsConfiguration()
     }
 
     @IBAction func nextButtonTaped() {
+        nextButton.configuration?.title = "NEXT"
+        nextButton.configuration?.attributedTitle?.font = .boldSystemFont(ofSize: 30)
+        
+        switch activeLight {
+        case .red:
+            activeLight = .yellow
+            redLightLabel.alpha = lightOn
+            greenLightLabel.alpha = lightOff
+        case .yellow:
+            activeLight = .green
+            yellowLightLabel.alpha = lightOn
+            redLightLabel.alpha = lightOff
+        case .green:
+            activeLight = .red
+            greenLightLabel.alpha = lightOn
+            yellowLightLabel.alpha = lightOff
+        }
     }
 
     private func nextButtonConfiguration() -> UIButton.Configuration {
         var button = UIButton.Configuration.filled()
         button.baseBackgroundColor = #colorLiteral(red: 0.2017898262, green: 0.200314194, blue: 1, alpha: 1)
-        button.title = "NEXT"
-        button.attributedTitle?.font = UIFont.boldSystemFont(ofSize: 30)
+        button.title = "START"
+        button.attributedTitle?.font = .boldSystemFont(ofSize: 30)
         button.cornerStyle = .medium
         button.buttonSize = .large
         return button
     }
 
-    private func ligthsConfiguration() {
-        for light in lightsLabels {
-            light.layer.cornerRadius = light.frame.width / 2
-            light.alpha = 0.3
-        }
+    private func initialViewsConfiguration() {
+        redLightLabel.backgroundColor = UIColor(red: 1, green: 0.149, blue: 0, alpha: 1)
+        yellowLightLabel.backgroundColor = UIColor(red: 1, green: 0.986, blue: 0, alpha: 1)
+        greenLightLabel.backgroundColor = UIColor(red: 0.447, green: 0.961, blue: 0.290, alpha: 1)
+
+        redLightLabel.alpha = lightOff
+        yellowLightLabel.alpha = lightOff
+        greenLightLabel.alpha = lightOff
+
+        redLightLabel.layer.cornerRadius = redLightLabel.frame.height / 2
+        yellowLightLabel.layer.cornerRadius = redLightLabel.layer.cornerRadius
+        greenLightLabel.layer.cornerRadius = redLightLabel.layer.cornerRadius
     }
 }
+
+
 
